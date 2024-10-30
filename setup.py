9ca9849da8642679a07bbd24ec1f691cf566c6d9
Anton Shutik <as@grove.co>
2024-10-30 13:29:47 +0100

    Removed unsupported python versions

--- setup.py
+++ setup.py
@@ -24,11 +24,9 @@ setup(
             "pre-commit"
         ]
     },
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
